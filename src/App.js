45b3aa313311e7709906f73a4cd9a87ecd0051ef
feng-coder <83321185+feng-coder@users.noreply.github.com>
2021-10-21 22:47:31 -0400

    add a line

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return <div>Hello to my first java script page  {value}</div>;
 }
 
 export default App;
